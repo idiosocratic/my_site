416036fb9a9814d7d542376953a2c9322ef7fcec
idiosocratic <david.baugher@idiosocratic.com>
2016-03-10 22:33:07 -0600

    Added wsgi application

--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
+import os
 import sys
 
 from django.conf.urls import url
 from django.http import HttpResponse
+from django.core.wsgi import get_wsgi_application
 
 def index(request):
   return HttpResponse('hello world')
@@ -12,10 +14,16 @@ urlpatterns = (
 
 from django.conf import settings
 
+DEBUG = os.environ.get('DEBUG', 'on') == 'on'
+
+SECRET_KEY = os.environ.get('SECRET_KEY', os.urandom(32))
+
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
 
 settings.configure(
-  DEBUG = True,
-  SECRET_KEY='thisisthesecretkey',
+  DEBUG = DEBUG,
+  SECRET_KEY = SECRET_KEY,
+  ALLOWED_HOSTS= ALLOWED_HOSTS,
   ROOT_URLCONF=__name__,
   MIDDLEWARE_CLASSES=(
     'django.middleware.common.CommonMiddleware',
@@ -23,6 +31,8 @@ settings.configure(
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     ),
 )
+
+application = get_wsgi_application()
 
 if __name__=="__main__":
   from django.core.management import execute_from_command_line
