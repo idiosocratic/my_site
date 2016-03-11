a8b70018ddaa2d11f8d8c854f947455bac2943eb
idiosocratic <david.baugher@idiosocratic.com>
2016-03-11 10:22:45 -0600

    Added templates and static directory

--- idiosocratic_site/idiosocratic_site/urls.py
+++ idiosocratic_site/idiosocratic_site/urls.py
@@ -3,8 +3,10 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'idiosocratic_site.views.home', name='home'),
+    url(r'^$', 'idiosocratic_site.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^image/(?P<width>[0-9]+)x(?P<height>[0-9]+)/$', placeholder, 
+        name = 'placeholder'),
 )
